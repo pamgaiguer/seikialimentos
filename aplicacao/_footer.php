4e63bed4df826b703d8b809fb759ef786a73eead
Pam Gaiguer <pamella.gaiguer@gmail.com>
2015-07-19 19:14:37 -0300

    footer and home are okay

--- aplicacao/_footer.php
+++ aplicacao/_footer.php
@@ -1,11 +1,18 @@
 <footer>
   <div class="container">
-    <address>
-      <strong>Endereço: </strong>Rua São Paulo, 43, Vila Tijuco - Guarulhos/SP<br>
-      <strong>Telefone: </strong><a href="tel:551124091383">+55 11 2409-1383</a><br>
-      <strong>E-mail: </strong><a href="mailto:seiki.alim@gmail.com">seiki.alim@gmail.com</a><br>
-    </address>
-
+    <div class="row">
+      <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+       <address>
+        <strong>Endereço: </strong>Rua São Paulo, 43, Vila Tijuco - Guarulhos/SP<br>
+        <strong>Telefone: </strong><a href="tel:551124091383">+55 11 2409-1383</a><br>
+        <strong>E-mail: </strong><a href="mailto:seiki.alim@gmail.com">seiki.alim@gmail.com</a><br>
+      </address>
+      <p>Seiki Alimentos &copy; 2015 - Todos os direitos reservados.</p>
+    </div>
+    <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+      <p class="copyright-text text-right">Ilustração por <a href="http://www.edsonyassuo.com.br">Edson Yassuo</a> e desenvolvido por <a href="http://www.pamellagaiguer.com" target="_self">Pamella Gaiguer</a></p>
+      </div>
+    </div>
   </div>
 </footer>
 
