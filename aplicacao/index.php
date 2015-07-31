7c1603bfee12b86281536c1749806c98072c5575
Pam Gaiguer <pamella.gaiguer@gmail.com>
2015-07-30 23:32:30 -0300

    gallery on modal okay - starting send mail topic

--- aplicacao/index.php
+++ aplicacao/index.php
@@ -1,10 +1,8 @@
 <?php include "_header.php"; ?>
 
-
-  <div class="container">
-    <div  class="main-banner banner-home">
-      <div class="carousel-inner">
-      </div>
+<div class="container">
+  <div  class="main-banner banner-home">
+    <div class="carousel-inner">
     </div>
   </div>
 </div>
@@ -28,19 +26,20 @@
       <p><a class="btn btn-success" href="producao.html">Ver mais »</a></p>
     </div>
   </div><!-- /.row -->
+</div>
+<hr class="featurette-divider">
 
-  <hr class="featurette-divider">
-
-  <div class="container">
-    <div class="col-md-7">
-      <h3 class="green-title">Seiki Alimentos</h3>
-      <p class="text-justify">A Seiki Alimentos Naturais é uma empresa brasileira, criada em 1999. Atua no mercado apícola, focado na exportação de própolis bruta.
+<div class="container">
+  <div class="col-md-7">
+    <h3 class="green-title">Seiki Alimentos</h3>
+    <p class="text-justify">A Seiki Alimentos Naturais é uma empresa brasileira, criada em 1999. Atua no mercado apícola, focado na exportação de própolis bruta.
       Está situada em Guarulhos, desfrutando da logística entre o Aeroporto Internacional de Guarulhos e da proximidade com a cidade de São Paulo.</p>
     </div>
     <div class="col-md-5">
 
-      <a data-toggle="modal" data-target="#myModal" id="linkModal">
-        <img src="images/gallery/galeria1_menor.jpg" alt="modal" class="img-responsive">
+      <a data-toggle="modal" data-target="#gallery" id="linkModal">
+        <img src="images/gallery/galeria1_menor.jpg" alt="modal" class="img-responsive img-modal">
+        <p class="text-center green-title">Clique aqui para ver a galeria</p>
       </a>
 
     </div>
@@ -48,7 +47,7 @@
 </div>
 
 <!-- Modal -->
-<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+<div class="modal fade" id="gallery" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
@@ -56,42 +55,51 @@
       </div>
       <div class="modal-body">
         <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-          <!-- Indicators -->
-          <ol class="carousel-indicators">
-            <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-            <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-            <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-            <li data-target="#carousel-example-generic" data-slide-to="3"></li>
-          </ol>
-
-          <!-- Wrapper for slides -->
           <div class="carousel-inner" role="listbox">
             <div class="item active">
-              <img src="images/gallery/galeria1_menor.jpg" alt="colmeia" class="center-block img-responsive">
-              <div class="carousel-caption">
-                <p>Colméia (beehive)</p>
+              <div class="panel panel-success">
+                <div class="panel-body">
+                  <img src="images/gallery/galeria1_menor.jpg" alt="colmeia" class="center-block img-responsive">
+                </div>
+                <div class="panel-footer">
+                  <h4 class="text-center green-title">Colméia (beehive)</h4>
+                </div>
               </div>
             </div>
+
             <div class="item">
-              <img src="images/gallery/galeria2_menor.jpg" alt="alecrim" class="center-block img-responsive">
-              <div class="carousel-caption">
-                <p>Alecrim-do-campo <em>(Baccharis dracuncunlifolia)</em></p>
+              <div class="panel panel-success">
+                <div class="panel-body">
+                  <img src="images/gallery/galeria2_menor.jpg" alt="alecrim" class="center-block img-responsive">
+                </div>
+                <div class="panel-footer">
+                  <h4 class="text-center green-title">Alecrim-do-campo <em>(Baccharis dracuncunlifolia)</em></h4>
+                </div>
               </div>
             </div>
+
             <div class="item">
-              <img src="images/gallery/galeria3_menor.jpg" alt="apiario" class="center-block img-responsive">
-              <div class="carousel-caption">
-                <p>Apiário (Apiary)</p>
+              <div class="panel panel-success">
+                <div class="panel-body">
+                  <img src="images/gallery/galeria3_menor.jpg" alt="apiario" class="center-block img-responsive">
+                </div>
+                <div class="panel-footer">
+                <h4 class="text-center green-title">Apiário (Apiary)</h4>
+                </div>
               </div>
             </div>
+
             <div class="item">
-              <img src="images/gallery/galeria4.JPG" alt="propolis" class="center-block img-responsive">
-              <div class="carousel-caption">
-                <p>Retirada de própolis da caixa (Removing propolis from the hive)</p>
+              <div class="panel panel-success">
+                <div class="panel-body">
+                  <img src="images/gallery/galeria4.JPG" alt="propolis" class="center-block img-responsive">
+                  <div class="panel-footer">
+                  <h5 class="text-center green-title">Retirada de própolis da caixa (Removing propolis from the hive)</h5>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-
           <!-- Controls -->
           <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
             <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
@@ -108,5 +116,6 @@
       </div>
     </div>
   </div>
+</div>
 </div>
 <?php include "_footer.php"; ?>
