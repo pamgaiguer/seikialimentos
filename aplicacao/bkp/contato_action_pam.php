5a0a8128ddaa4988c130f8704b82f7b7f38b5452
Pamella Gaiguer <pamella.gaiguer@gmail.com>
2015-08-18 10:09:31 -0300

    fixing mail with phpmailer

--- aplicacao/bkp/contato_action_pam.php
+++ aplicacao/bkp/contato_action_pam.php
@@ -1,8 +1,10 @@
 <?php
 
-include_once ("class.phpmailer.php");
-//include_once ("class.pop3.php");
-include_once ("class.smtp.php");
+require 'phpmailer/class.phpmailer.php';
+require 'phpmailer/class.smtp.php';
+
+$from = 'contato@seikialimentos.com.br';
+$fromName = 'Contato Site';
 
 $message = "";
 
@@ -19,10 +21,14 @@ $Host = 'smtp.seikialimentos.com.br';
 $Username = 'contato';
 $Password = 'Propolis@12';
 $Port = "587";
+$Secure = '';
 
 $mail = new PHPMailer();
 $body = $Message;
+$mail->isHTML(true);
+$mail->CharSet = 'utf-8';
 $mail->IsSMTP(); // telling the class to use SMTP
+$mail->setLanguage('pt', 'phpmailer/language/phpmailer.lang-pt.php');
 $mail->Host = $Host; // SMTP server
 $mail->SMTPDebug = 0;
 $mail->SMTPAuth = true; // enable SMTP authentication
@@ -30,10 +36,11 @@ $mail->Port = $Port; // set the SMTP port for the service server
 $mail->Username = $Username; // account username
 $mail->Password = $Password; // account password
 
-$mail->SetFrom($usuario, $To);
+$mail->From = $from;
+$mail->FromName = $fromName;
 $mail->Subject = $Subject;
 $mail->MsgHTML($body);
-$mail->AddAddress($To, "");
+$mail->AddAddress($To, 'Contato');
 
 return $mail->Send();
 
